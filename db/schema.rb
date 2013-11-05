9a0399bc99c01ee21a7592def985e8c2cb819ee2
Nejuf <nejuf1@gmail.com>
2013-11-05 14:35:09 -0800

    Fixes Devise sign up

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20131105215138) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
