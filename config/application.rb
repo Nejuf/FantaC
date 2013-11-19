40dcefed8d854b5900b4d0847995da977484bf25
Nejuf <nejuf1@gmail.com>
2013-11-18 22:22:21 -0800

    Fixes redis dev setup

--- config/application.rb
+++ config/application.rb
@@ -75,21 +75,5 @@ module Fantac
     #       end
     #     end
 
-
-    #Redis
-    if ENV["REDISTOGO_URL"]
-      config = Fantac::Application.config
-      uri = URI.parse(ENV["REDISTOGO_URL"])
-
-      config.cache_store = [
-        :redis_store, {
-          :host => uri.host,
-          :port => uri.port,
-          :password => uri.password,
-          :namespace => "cache"
-        }
-      ]
-    end
-
   end
 end
