2c15bb0c38ea2683fa967f7345f4220f08b98a23
Nejuf <nejuf1@gmail.com>
2014-01-11 11:20:46 -0800

    Adds Affinity model validation specs

--- spec/factories/affinities.rb
+++ spec/factories/affinities.rb
@@ -1,7 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+	sequence(:aff_name) do |n|
+		"MyAffinity#{n}"
+	end
+
   factory :affinity do
-    name "MyString"
+    name { generate(:aff_name) }
   end
 end
